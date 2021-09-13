8e6b2a03bcf3dc4569c0ff1d23dea9f00fb3b4af
J00n4 <63159327+J00n4@users.noreply.github.com>
2021-09-13 18:36:45 +0300

    Update1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.getElementById("app").innerHTML = `
 
 `;
 
-// Exercise 3 starts
+// Exercise 3 & 4 starts
 if (document.readyState !== "loading") {
   console.log("Document is ready");
   initializeCode();
@@ -23,37 +23,12 @@ function initializeCode() {
     console.log("working!");
 
     let newParagraph = document.createElement("li");
-    newParagraph.innerHTML = newParagraph.innerHTML + "new list item";
+    newParagraph.innerHTML = document.getElementById("big-text").value;
 
     commentsList.appendChild(newParagraph);
   });
 }
-// Exercise 3 ends
-
-// Exercise 4 starts
-if (document.readyState !== "loading") {
-  console.log("Document is ready");
-  addToComments();
-} else {
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("Document ready after waiting");
-    addToComments();
-  });
-}
-
-function addToComments() {
-  const addTextButton = document.getElementById("add-text");
-
-  addTextButton.addEventListener("click", function () {
-    const commentsList2 = document.getElementById("comments");
-    console.log("working!");
-
-    var newParagraph2 = document.createElement("li");
-    newParagraph2.innerHTML = document.getElementById("big-text").value;
-    commentsList2.appendChild(newParagraph2);
-  });
-}
-// Exercise 4 ends
+// Exercise 3 & 4 ends
 
 // Exercise 5 starts
 if (document.readyState !== "loading") {
@@ -75,7 +50,6 @@ function removeComments() {
     );
     if (response == true) {
       document.getElementById("comments-list").innerHTML = "";
-      document.getElementById("comments").innerHTML = "";
     } else {
       document.getElementById("output").innerHTML = "You canceled.";
     }
